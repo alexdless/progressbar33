d1a5a1a8b7499741c0fd3459a4e27ee7adf74824
alexdless <alexdless0@gmail.com>
2016-08-25 03:09:01 +0300

    .
    .

---
+++ test.py
@@ -0,0 +1,15 @@
+from progressbar import ProgressBar
+
+import time
+
+pbar = ProgressBar(maxval=10)
+
+pbar.start()
+
+for i in range(1, 11):
+
+    pbar.update(i)
+
+    time.sleep(1)
+
+pbar.finish()
